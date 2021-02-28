de765df3764be722cafbf1d27c74a8ba45d76160
Logan Engstrom <engstrom@mit.edu>
2021-02-28 00:40:52 -0500

    added

---
+++ cli.py
@@ -0,0 +1,100 @@
+"""GitLinks: git-based shortlinks. See: https://github.com/lengstrom/gitlinks.
+
+Usage:
+  gitlinks init <url>
+  gitlinks set <key> <url>
+  gitlinks delete <key>
+  gitlinks show
+
+Options:
+  -h --help     Show this screen.
+  -v --version  Show version.
+"""
+from ilock import ILock
+from docopt import docopt
+from pathlib import Path
+import shutil
+import json
+import git
+import pandas as pd
+from utils import clone, query_yes_no, try_setup, serialize_csv, load_csv
+from utils import commit_push, check_repo, generate_pages
+
+GIT_PATH = Path('~/.gitlinks/').expanduser()
+INDEX_NAME = 'index.csv'
+
+def initialize(url, path=GIT_PATH):
+    if path.exists():
+        if query_yes_no(f'{path} already exists; really delete?', default='yes'):
+            shutil.rmtree(path)
+        else:
+            print('Ok, exiting...')
+            sys.exit()
+
+    repo = clone(url, path)
+    try_setup(repo, path, INDEX_NAME)
+    print(f'Successfully initialized gitlinks to url: {url}')
+
+def set_link(key, url, df):
+    df = df[df.key != key]
+    df = df.append(pd.Series({
+        'key':key,
+        'url':url
+    }), ignore_index=True)
+
+    return df
+
+def delete_link(key, df):
+    return df[df.key != key]
+
+def show(df, repo):
+    df['->'] = ['->' for _ in range(df.shape[0])]
+
+    new_order = [0, 2, 1]
+    df = df[df.columns[new_order]]
+    df = df.rename(lambda x:x.upper(), axis=1)
+    print(df.to_string(index=False)) #, justify=['right', 'center', 'left']))
+    print('')
+    print(f'Git Remote: {repo.remotes.origin.url}')
+
+def main(args):
+    if args['init']:
+        return initialize(args['<url>'])
+
+    repo = git.Repo(GIT_PATH)
+    if not check_repo(repo, INDEX_NAME):
+        msg = "No initialized repo; run `gitlinks init <url>` first!"
+        raise ValueError(msg)
+
+    csv_path = GIT_PATH / INDEX_NAME
+    df = load_csv(csv_path)
+
+    if args['show']:
+        return show(df, repo)
+
+    if args['set']:
+        key = args['<key>']
+        url = args['<url>']
+        df = set_link(key, url, df)
+        commit_msg = f'Set {key} -> {url}'
+    elif args['delete']:
+        key = args['<key>']
+        df = delete_link(key, df)
+        commit_msg = f'Removed {key}'
+
+    serialize_csv(df, csv_path)
+    generate_pages(df, GIT_PATH, INDEX_NAME)
+
+    try:
+        commit_push(repo, commit_msg[:50])
+        print(f'Success: {commit_msg}')
+    except Exception as e:
+        repo.git.reset('--hard','origin/master')
+        print(f'Failed; rolling back')
+        raise e
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+
+    with ILock('gitlinks'):
+        main(args)
---
+++ utils.py
@@ -0,0 +1,130 @@
+import git
+import tqdm
+import pandas as pd
+from git import RemoteProgress
+import shutil
+import sys
+from pathlib import Path
+
+def empty_csv():
+    return pd.DataFrame({
+        'key':[],
+        'url':[]
+    })
+
+def serialize_csv(df, path):
+    df.to_csv(str(path), index=False)
+
+def load_csv(f):
+    return pd.read_csv(str(f))
+
+class CloneProgress(RemoteProgress):
+    def update(self, op_code, cur_count, max_count=None, message=''):
+        if message:
+            print(message)
+
+def clone(url, path):
+    prog = CloneProgress()
+    return git.Repo.clone_from(url, path, progress=prog)
+
+import traceback
+
+def commit_push(repo, commit_msg):
+    repo.git.add(all=True)
+    repo.index.commit(commit_msg)
+    origin = repo.remote(name='origin')
+    origin.push()
+
+def clean(repo):
+    repo.git.clean('-xdf')
+
+def check_repo(repo, index_name):
+    wd = repo.working_dir
+    try:
+        load_csv(str(Path(wd) / index_name))
+        return True
+    except:
+        return False
+
+def wipe_directory(dired, protected):
+    protected = set(protected)
+    for child in dired.iterdir():
+        if child.name not in protected:
+            if child.is_dir():
+                shutil.rmtree(child)
+            else:
+                child.unlink()
+
+def try_setup(repo, path, index_name):
+    # path is the path of a git repository
+    # Clean up any non-tracked changes
+    # Then check if there's an index.csv; if so, exit
+    # If no index.csv, delete everything in repo, make an empty index.csv
+    clean(repo)
+    index_path = path / index_name
+
+    if not check_repo(repo, index_name):
+        print('Index not found; initializing index!')
+        # delete everything in directory
+        wipe_directory(path, ['.git'])
+
+        empty = empty_csv()
+        serialize_csv(empty, index_path)
+        try:
+            commit_push(repo, 'Initialization')
+        except Exception as e:
+            print('Remote update failed; try initializing again!')
+            raise e
+
+def template_maker(url):
+    return f'<meta http-equiv="refresh" content="0; URL={url}"/>'
+
+def generate_pages(df, working_dir, index_name):
+    wd = Path(working_dir)
+    protected = ['.git', index_name]
+    wipe_directory(wd, protected)
+
+    parent_cache = set()
+    for _, row in df.iterrows():
+        key, url = row.key, row.url
+        html_file = wd / (key + '.html')
+        parent = html_file.parent
+        if not parent in parent_cache:
+            parent.mkdir(exist_ok=True)
+
+        parent_cache.add(parent)
+        with open(html_file, 'w+') as f:
+            f.write(template_maker(url))
+ 
+# This is from StackOverflow.
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
