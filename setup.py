90246370aa459b6ae0bd4e29158f118226bd1a33
Logan Engstrom <engstrom@mit.edu>
2021-03-01 00:04:54 -0500

    added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='gitlinks',  
-    version='0.1',
+    version='0.1.1',
     author='Logan Engstrom',
     author_email='logan@mit.edu',
     description='GitHub pages-powered shortlinks.',
